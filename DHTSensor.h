1b3b65da44799de3f4850d4b039802f8e6521742
Claudiu Matei <claudiu@shmessenger.org>
2016-08-08 13:36:45 +0300

    Added constructor for DHTSensor

--- DHTSensor.h
+++ DHTSensor.h
@@ -7,10 +7,17 @@
 
 #include <Arduino.h>
 #include <DHT.h>
-#include <Print.h>
 #include <FlowerPlatformArduinoRuntime.h>
+#include <Print.h>
+#include <WString.h>
 
 class DHTSensor {
+protected:
+	uint8_t pin;
+	unsigned long lastTime;
+	double lastTemperature;
+	double lastHumidity;
+
 public:
 
 	// TODO CS: TEMP
@@ -19,70 +26,79 @@ public:
 	Callback<ValueChangedEvent>* onTemperatureChanged = NULL;
 	Callback<ValueChangedEvent>* onHumidityChanged = NULL;
 
-	uint8_t pin;
+	/*
+	 * @flower { constructorVariant="Default" }
+	 */
+	DHTSensor(uint8_t pin);
+
 	unsigned int pollInterval;
 
-	void setup() {
-		lastTime = 0;
-		lastHumidity = -1000;
-		lastTemperature = -1000;
-		pinMode(pin, INPUT);
+	void setup();
 
-		if (pollInterval <= 0) {
-			pollInterval = 1000;
-		}
-	}
+	void loop();
 
-	void loop() {
-		if ((unsigned long int)(millis() - lastTime) < pollInterval) {
-			return;
-		}
+	void printStateAsJson(const __FlashStringHelper* instanceName, Print* print);
 
-		lastTime = millis();
-
-		DHT dhtSensor(pin, DHT11);
-
-		double temperature = dhtSensor.readTemperature();
-		if (temperature != lastTemperature && onTemperatureChanged != NULL) {
-			ValueChangedEvent event;
-			event.currentValue = temperature;
-			event.previousValue = lastTemperature;
-			(*onTemperatureChanged)(&event);
-		}
-		lastTemperature = temperature;
-
-		double humidity = dhtSensor.readHumidity();
-		if (humidity != lastHumidity && onHumidityChanged != NULL) {
-			ValueChangedEvent event;
-			event.currentValue = humidity;
-			event.previousValue = lastHumidity;
-			(*onHumidityChanged)(&event);
-		}
-		lastHumidity = humidity;
-	}
-
-	void printStateAsJson(const __FlashStringHelper* instanceName, Print* print) {
-
-		print->print(F("\""));
-		print->print(instanceName);
-		print->print(F("__temperature"));
-		print->print(F("\":"));
-		print->print(lastTemperature);
-
-		print->print(F(","));
-
-		print->print(F("\""));
-		print->print(instanceName);
-		print->print(F("__humidity"));
-		print->print(F("\":"));
-		print->print(lastHumidity);
-	}
-
-
-protected:
-	unsigned long lastTime;
-	double lastTemperature;
-	double lastHumidity;
 };
+
+DHTSensor::DHTSensor(uint8_t pin) {
+	this->pin = pin;
+};
+
+void DHTSensor::setup() {
+	lastTime = 0;
+	lastHumidity = -1000;
+	lastTemperature = -1000;
+	pinMode(pin, INPUT);
+
+	if (pollInterval <= 0) {
+		pollInterval = 1000;
+	}
+}
+
+void DHTSensor::loop() {
+	if ((unsigned long int)(millis() - lastTime) < pollInterval) {
+		return;
+	}
+
+	lastTime = millis();
+
+	DHT dhtSensor(pin, DHT11);
+
+	double temperature = dhtSensor.readTemperature();
+	if (temperature != lastTemperature && onTemperatureChanged != NULL) {
+		ValueChangedEvent event;
+		event.currentValue = temperature;
+		event.previousValue = lastTemperature;
+		(*onTemperatureChanged)(&event);
+	}
+	lastTemperature = temperature;
+
+	double humidity = dhtSensor.readHumidity();
+	if (humidity != lastHumidity && onHumidityChanged != NULL) {
+		ValueChangedEvent event;
+		event.currentValue = humidity;
+		event.previousValue = lastHumidity;
+		(*onHumidityChanged)(&event);
+	}
+	lastHumidity = humidity;
+}
+
+void DHTSensor::printStateAsJson(const __FlashStringHelper* instanceName, Print* print) {
+
+	print->print(F("\""));
+	print->print(instanceName);
+	print->print(F("__temperature"));
+	print->print(F("\":"));
+	print->print(lastTemperature);
+
+	print->print(F(","));
+
+	print->print(F("\""));
+	print->print(instanceName);
+	print->print(F("__humidity"));
+	print->print(F("\":"));
+	print->print(lastHumidity);
+}
 
 #endif
